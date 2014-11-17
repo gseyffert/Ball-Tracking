e74f7b0e7513b253dfe9443753d304ee206fd801
Graham Seyffert <gseyffert@gmail.com>
2014-11-17 14:08:27 -0800

    Header added

---
+++ BallTracking.h
@@ -0,0 +1,48 @@
+typedef struct {
+	int frameNumber;	
+	int candidateNum;
+	edge* list;
+} node;
+
+typedef struct {
+	node* to;
+	node* from;
+	int weight(euclidian dist?);
+} edge;
+
+typedef struct  {
+	int x;
+	int y;
+	int radius;
+	int probability;
+} candidate;
+
+typedef struct {
+	candidate* list;
+	uint64_t numCandidates;
+} frame;
+
+listOfFrames;
+listofCandidates;
+
+graph composeGraph(*listOfFrames) {
+	Graham
+}
+
+frame ballDetect(.jpg, [threshholds]) {
+	Max + Myles
+}
+
+listOfCandidates shortestPath(graph) {
+	Evan
+}
+
+mp4 visualize(listOfCandidates, srcVid.mp4) {
+	Avi
+}
+
+main(int argc, int **argv[]) {
+	David
+}
+
+
