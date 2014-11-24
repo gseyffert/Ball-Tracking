4565b1cae6f3c89b8edbbb429fec38b62eae3f04
Graham Seyffert <gseyffert@berkeley.edu>
2014-11-23 16:26:43 -0800

    Update composeGraph.cpp

--- graph/composeGraph.cpp
+++ graph/composeGraph.cpp
@@ -16,6 +16,8 @@ node* composeGraph(frame* frameArray, int numFrames){
     // The source node of the graph (doesnt represent a frame), links to all candidates in the first frame
     node* source = new node();
     source->isStart = true;
+    source->numEdges = frameArray[0]->numCandidates;
+    source->edgeList = new edge[source->numEdges]
 
     // At the beginning the last frame is the source
     node* lastFrame = source;
@@ -23,40 +25,57 @@ node* composeGraph(frame* frameArray, int numFrames){
     // RIGHT NOW THIS ONLY WORKS FOR THE FIRST FRAME SINCE THERE IS ONLY ONE NODE (the source) BEFORE FRAME 0
     // FOR OTHER FRAMES WE NEED TO MAKE AN EDGE BETWEEN EVERY NODE IN THE PREVIOUS FRAME AND THE CURRENT FRAME
 
-    // Go through each frame
-    for(int i=0; i < numFrames; i++){
-        frame* curFrame = &frameArray[i];
-        int numCandidates = curFrame->numCandidates;
 
+    //Initialize variables
+    frame* curFrame, prevFrame = &frameArray[0];
+    int numCandidates, numPrevCandidates = 1;
+    candidate* curCand, prevCand, candidateArray;
+
+    // Go through each frame
+    for(int i = 0; i < numFrames; i++) {
+        if (i != 0) {
+            curFrame = &frameArray[i];
+            numCandidates = curFrame->numCandidates;
+            prevFrame = frameArray[i-1];
+            numPrevCandidates = prevFrame->numCandidates;
+        }
+        candidate* candidateArray = curFrame->candidateList;
         cout << "Frame " << i << " has " << numCandidates << " ball candidates." << endl;
 
-        // Allocate the edges that will point from the previous frame to this frame
-        edge* edgesToThisFrame = new edge[numCandidates];
-        lastFrame->numEdges = numCandidates;
+        // Go through each candidate of current frame and create a node for it
+        // Also set edges from the last frame candidates to this candidate
+        prevCands = prevFrame->
+        for(int j = 0; j < numCandidates; j++) {
+            //cout << "    Candidate " << j << " coords:(" << curCand->x << "," << curCand->y << ") confidence:" << curCand->probability << endl;
+            //cout << "    created edge (node) " << *(edgesToThisFrame[j].start) << " to (node) " << *(edgesToThisFrame[j].end) << endl << endl;
+            
+            curCand = &candidateArray[j];
+            edge* edgesToThisCand = new edge[prevCandidates];
+            
+            
+            //Link source to first frame
+            if (i = 0) {
+                edge temp = new edge(source, );
+                source->edgeList[j] = temp;
+                
+            } else {
+                
 
-        candidate* candidateArray = curFrame->candidateList;
-        // Go through each candidate of that frame and create a node for it
-        // Also set one of the edges from the last frame candidates to this candidate
-        for(int j=0; j<numCandidates; j++){
-            candidate* curCand = &candidateArray[j];
-            cout << "    Candidate " << j << " coords:(" << curCand->x << "," << curCand->y << ") confidence:" << curCand->probability << endl;
-
-            // Allocate node for this candidate
-            node* thisCand = new node(i, j, curCand);
-            // cout << "This cand: " << *thisCand << endl;
-
-            edge* edgeToThisCand = &edgesToThisFrame[j];
-            edgeToThisCand->start = lastFrame;
-            edgeToThisCand->end = thisCand;
-            edgeToThisCand->weight = curCand->probability;
-
-            cout << "    created edge (node) " << *(edgesToThisFrame[j].start) << " to (node) " << *(edgesToThisFrame[j].end) << endl << endl;
+                
+            
+                edge* edgeToThisCand = &edgesToThisFrame[j];
+                edgeToThisCand->start = lastFrame;
+                edgeToThisCand->end = thisCand;
+                edgeToThisCand->weight = curCand->probability;
+                
+            }
+           
         }
 
         // Set the edge list of the previous frame's nodes
         lastFrame->edgeList = edgesToThisFrame;
     }
-    return NULL;
+    return source;
 }
 
 /**
