98d282d2ee4489353a391acd144da5626f81c5d3
Evan <evanfossier@berkeley.edu>
2014-11-25 13:36:35 -0800

    changed freeGraph prototype

--- graph/freeGraph.cpp
+++ graph/freeGraph.cpp
@@ -1,10 +1,13 @@
 /**
  * Frees all the memory associated with a graph (edges and nodes)
  * After this is run all the frame->nodes pointers are null for each frame;
- */
+*/
 
 #include "../BallTracking.h"
+#include <iostream>
 
- void freeGraph(frame* listOfFrames, int numFrames){
-    
- }
+// Starts at end of frame array and frees nodes for that frame then frees edges that pointed to those nodes
+void freeGraph(frame* listOfFrames, node* graph, int numFrames){
+    cout << "Starting graph freeing" << endl;
+    // Start from end frame
+}
