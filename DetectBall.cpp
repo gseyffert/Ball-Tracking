355b667dcfa106d5d24c1f6a8c159f6cd4436d23
Max Pollard <mmpollard10@gmail.com>
2014-12-10 01:44:38 -0800

    OpenCL

--- DetectBall.cpp
+++ DetectBall.cpp
@@ -1,7 +1,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
-#include "opencv2/ocl/ocl.hpp"
+#include "ocl/ocl.hpp"
 #include "OpenCLUtilities/openCLUtilities.hpp"
 
 
